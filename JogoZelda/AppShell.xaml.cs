d00bb5ae14aacbb7b085c6e6289019294c042178
Samuel Burgrever Torres <samuel.burgrever@outlook.com>
2025-04-25 20:06:38 -0300

    Criando projeto para jogo zelda

---
+++ JogoZelda/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace JogoZelda
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ JogoZelda/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace JogoZelda;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
